e9daa3ec8ead5c7e5abf9cb9a5850bc4358d74c8
Mark Adrian Valencia <55659191+medievaldotcom@users.noreply.github.com>
2022-02-18 02:03:22 +0800

    Qr coder completed
    Ability to scan qr and save them as text files to database. allow option for qr to be upload if no webcam is present

--- QRCoder/Form1.Designer.cs
+++ QRCoder/Form1.Designer.cs
@@ -28,25 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtQRCode = new System.Windows.Forms.TextBox();
+            this.components = new System.ComponentModel.Container();
+            this.txtQRCodeName = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.pic = new System.Windows.Forms.PictureBox();
+            this.txtQRCodeBirthdate = new System.Windows.Forms.TextBox();
+            this.txtQRCodeAddress = new System.Windows.Forms.TextBox();
+            this.txtQRCodePhoneNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtQRCodeEmailAddress = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.choosedevice = new System.Windows.Forms.ComboBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.QRtext = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Start = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // txtQRCode
+            // txtQRCodeName
             // 
-            this.txtQRCode.Location = new System.Drawing.Point(25, 168);
-            this.txtQRCode.Multiline = true;
-            this.txtQRCode.Name = "txtQRCode";
-            this.txtQRCode.Size = new System.Drawing.Size(208, 57);
-            this.txtQRCode.TabIndex = 0;
+            this.txtQRCodeName.Location = new System.Drawing.Point(767, 280);
+            this.txtQRCodeName.Multiline = true;
+            this.txtQRCodeName.Name = "txtQRCodeName";
+            this.txtQRCodeName.Size = new System.Drawing.Size(227, 29);
+            this.txtQRCodeName.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(74, 231);
+            this.button1.Location = new System.Drawing.Point(1000, 463);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(118, 43);
+            this.button1.Size = new System.Drawing.Size(94, 43);
             this.button1.TabIndex = 1;
             this.button1.Text = "Generate QR";
             this.button1.UseVisualStyleBackColor = true;
@@ -54,24 +74,203 @@
             // 
             // pic
             // 
-            this.pic.Location = new System.Drawing.Point(25, 12);
+            this.pic.Location = new System.Drawing.Point(12, 25);
             this.pic.Name = "pic";
-            this.pic.Size = new System.Drawing.Size(208, 150);
+            this.pic.Size = new System.Drawing.Size(383, 481);
             this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pic.TabIndex = 2;
             this.pic.TabStop = false;
+            // 
+            // txtQRCodeBirthdate
+            // 
+            this.txtQRCodeBirthdate.Location = new System.Drawing.Point(767, 328);
+            this.txtQRCodeBirthdate.Multiline = true;
+            this.txtQRCodeBirthdate.Name = "txtQRCodeBirthdate";
+            this.txtQRCodeBirthdate.Size = new System.Drawing.Size(227, 29);
+            this.txtQRCodeBirthdate.TabIndex = 3;
+            // 
+            // txtQRCodeAddress
+            // 
+            this.txtQRCodeAddress.Location = new System.Drawing.Point(767, 376);
+            this.txtQRCodeAddress.Multiline = true;
+            this.txtQRCodeAddress.Name = "txtQRCodeAddress";
+            this.txtQRCodeAddress.Size = new System.Drawing.Size(227, 29);
+            this.txtQRCodeAddress.TabIndex = 4;
+            // 
+            // txtQRCodePhoneNumber
+            // 
+            this.txtQRCodePhoneNumber.Location = new System.Drawing.Point(767, 425);
+            this.txtQRCodePhoneNumber.Multiline = true;
+            this.txtQRCodePhoneNumber.Name = "txtQRCodePhoneNumber";
+            this.txtQRCodePhoneNumber.Size = new System.Drawing.Size(227, 29);
+            this.txtQRCodePhoneNumber.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(764, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(764, 312);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Date of Birth";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(764, 360);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(764, 409);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Phone No.";
+            // 
+            // label5
+            // 
+            this.label5.AccessibleName = "txtQRCodeEmail";
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(764, 457);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Email Address";
+            // 
+            // txtQRCodeEmailAddress
+            // 
+            this.txtQRCodeEmailAddress.AccessibleName = "txtQRCodeEmail";
+            this.txtQRCodeEmailAddress.Location = new System.Drawing.Point(767, 473);
+            this.txtQRCodeEmailAddress.Multiline = true;
+            this.txtQRCodeEmailAddress.Name = "txtQRCodeEmailAddress";
+            this.txtQRCodeEmailAddress.Size = new System.Drawing.Size(227, 29);
+            this.txtQRCodeEmailAddress.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(86, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "QR Generator";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(401, 41);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Camera:";
+            // 
+            // choosedevice
+            // 
+            this.choosedevice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.choosedevice.FormattingEnabled = true;
+            this.choosedevice.Location = new System.Drawing.Point(453, 38);
+            this.choosedevice.Name = "choosedevice";
+            this.choosedevice.Size = new System.Drawing.Size(121, 21);
+            this.choosedevice.TabIndex = 15;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox.Location = new System.Drawing.Point(453, 65);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(302, 438);
+            this.pictureBox.TabIndex = 16;
+            this.pictureBox.TabStop = false;
+            // 
+            // QRtext
+            // 
+            this.QRtext.Location = new System.Drawing.Point(767, 54);
+            this.QRtext.Multiline = true;
+            this.QRtext.Name = "QRtext";
+            this.QRtext.Size = new System.Drawing.Size(327, 204);
+            this.QRtext.TabIndex = 17;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(578, 38);
+            this.Start.Margin = new System.Windows.Forms.Padding(2);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(88, 19);
+            this.Start.TabIndex = 18;
+            this.Start.Text = "Capture QR";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(670, 38);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 21);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Upload QR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.uploadQR);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(889, 38);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "QR Text";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1122, 518);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.QRtext);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.choosedevice);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtQRCodeEmailAddress);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtQRCodePhoneNumber);
+            this.Controls.Add(this.txtQRCodeAddress);
+            this.Controls.Add(this.txtQRCodeBirthdate);
             this.Controls.Add(this.pic);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.txtQRCode);
+            this.Controls.Add(this.txtQRCodeName);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "QR Master";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,9 +278,27 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox txtQRCode;
+        private System.Windows.Forms.TextBox txtQRCodeName;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.TextBox txtQRCodeBirthdate;
+        private System.Windows.Forms.TextBox txtQRCodeAddress;
+        private System.Windows.Forms.TextBox txtQRCodePhoneNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtQRCodeEmailAddress;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox choosedevice;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox QRtext;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- QRCoder/Form1.cs
+++ QRCoder/Form1.cs
@@ -1,26 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing;
+using MessagingToolkit.QRCode.Codec.Data;
 
 namespace QRCoder
 {
     public partial class Form1 : Form
     {
+<<<<<<< HEAD
+
+
+=======
      
          
+>>>>>>> 4fcd0781e07fb647e947bebd50a52415dba457b4
         public Form1()
         {
             InitializeComponent();
         }
 
+
         private void GenerateQR(object sender, EventArgs e)
         {
+<<<<<<< HEAD
+            string time = DateTime.Now.ToString("hh-mm-ss-tt");
+            string date = DateTime.Now.ToString("yyyy-MM-d ");
+            string filename = txtQRCodeName.Text + " " + "Date " + date + "Time " + time + ".png";
+            string message = "Qr Code will be saved to database as image.";
+            string msgtitle = "QR saved date" + date + "saved at " + time;
+
+
+
+
+            QRCodeGenerator qr = new QRCodeGenerator();
+            QRCodeData data = qr.CreateQrCode("Name: " + txtQRCodeName.Text + "\n" + "Date of Birth: " + txtQRCodeBirthdate.Text + "\n" + "Address: " + txtQRCodeAddress.Text + "\n" + "Phone Number: " + txtQRCodePhoneNumber.Text + "\n" + "Email Address: " + txtQRCodeEmailAddress.Text, QRCodeGenerator.ECCLevel.Q);
+            QRCode code = new QRCode(data);
+            Bitmap bitMap = code.GetGraphic(5);
+=======
             string time = DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss-tt");
             string backup = String.Format(@"C:\Users\Adrian\Downloads\");
             string filename = time + ".png";
@@ -33,6 +54,7 @@ namespace QRCoder
 
          
 
+>>>>>>> 4fcd0781e07fb647e947bebd50a52415dba457b4
             using (MemoryStream ms = new MemoryStream())
             {
                 bitMap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
@@ -40,11 +62,107 @@ namespace QRCoder
                 pic.Height = bitMap.Height;
                 pic.Width = bitMap.Width;
             }
+<<<<<<< HEAD
+            bitMap.Save(@"C:\Users\Adrian\Documents\GitHub\QRCoder\Database\QrCodes\ " + filename, ImageFormat.Png);
+            MessageBox.Show(message, msgtitle);
+
+        }
+
+
+        ///====================================QR Generator End============================================================================///
+        ///
+        ///====================================QR scanner ============================================================================///
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+        QRCodeGenerator qr = new QRCodeGenerator();
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo Device in filterInfoCollection)
+                choosedevice.Items.Add(Device.Name);
+            choosedevice.SelectedIndex = 0;
+            videoCaptureDevice = new VideoCaptureDevice();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[choosedevice.SelectedIndex].MonikerString);
+            videoCaptureDevice.NewFrame += FinalFrame_NewFrame;
+            videoCaptureDevice.Start();
+            timer1.Start();
+        }
+        private void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox.Image = (Bitmap)eventArgs.Frame.Clone();
+=======
 
               
                 bitMap.Save(filename, ImageFormat.Png);
                 bitMap.Save(backup + filename, ImageFormat.Png);
                 MessageBox.Show(message, msgtitle);
+>>>>>>> 4fcd0781e07fb647e947bebd50a52415dba457b4
         }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+            string time = DateTime.Now.ToString("hh-mm-ss-tt");
+            string date = DateTime.Now.ToString("yyyy-MM-d ");
+            string filename = txtQRCodeName.Text + " " + "Date " + date + "Time " + time + ".txt";
+            string message = "Qr Code will be saved to database as text.";
+            string msgtitle = "QR saved date" + date + "saved at " + time;
+
+            BarcodeReader Reader = new BarcodeReader();
+            Result result = Reader.Decode((Bitmap)pictureBox.Image);
+            if (result != null)
+            {
+
+                QRtext.Text = result.ToString();
+                System.IO.File.WriteAllText(@"C:\Users\Adrian\Documents\GitHub\QRCoder\Database\QrCodeTextFiles\ " + filename, QRtext.Text);
+                MessageBox.Show(message, msgtitle);
+            }
+
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (videoCaptureDevice != null)
+                videoCaptureDevice.Stop();
+        }
+  
+
+        ///====================================QR Scanner End============================================================================///
+        ///==================================Upload QR if no Webcam Present==================================///
+        private void uploadQR(object sender, EventArgs e)
+        {
+            string time = DateTime.Now.ToString("hh-mm-ss-tt");
+            string date = DateTime.Now.ToString("yyyy-MM-d ");
+            string filename = txtQRCodeName.Text + " " +"Date "+ date + "Time "+ time + ".txt";
+            string message = "Qr Code will be saved to database as text.";
+            string msgtitle = "QR saved date" +date+ "saved at " + time;
+         
+
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "PNG|*.png", ValidateNames = true, Multiselect = false })
+            {
+             
+
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    //Decode qrcode from image
+                    pic.Image = Image.FromFile(ofd.FileName);
+                    MessagingToolkit.QRCode.Codec.QRCodeDecoder decoder = new MessagingToolkit.QRCode.Codec.QRCodeDecoder();
+                    QRtext.Text = decoder.Decode(new QRCodeBitmapImage(pic.Image as Bitmap));
+                    System.IO.File.WriteAllText(@"C:\Users\Adrian\Documents\GitHub\QRCoder\Database\QrCodeTextFiles\ " + filename, QRtext.Text);
+                    MessageBox.Show(message, msgtitle);
+                }
+
+              
+            }
+
+        }
+
+      
     }
 }
